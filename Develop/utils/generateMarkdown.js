3be4993fe520c3ba9b2adb2b8343ea6e53595dc6
ricky-rosay <111669764+ricky-rosay@users.noreply.github.com>
2023-01-18 20:33:14 -0500

    render license

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+  if (license) {
+    switch (license) {
+      case (license = "MIT"):
+        return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+      case (license = "Apache 2.0"):
+        return "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)";
+      case (license = "GNU GPL v3.0"):
+        return "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
+      case (license = "None"):
+        return "";
+    }
+  }
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
